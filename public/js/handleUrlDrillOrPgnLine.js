4f1e06fe994b76e09acd5185e6066dce44a33863
john-tennant-sjs <john.tennant@sweetjusticesound.com>
2023-05-01 22:12:25 -0700

    Change the way we're opening urls.

--- public/js/handleUrlDrillOrPgnLine.js
+++ public/js/handleUrlDrillOrPgnLine.js
@@ -3,28 +3,14 @@
 // if yes, then open a mPGNPlaybackChessboard.html and copy the 'string' to the clipboard
  
 export function openURLorPGN(string) {
-    if (isNaN(string.charAt(0))) {
+  if (isNaN(string.charAt(0))) {
+    window.location.href = string;
+  } else {
+    const pgnString = string;
+    const encodedPgnString = encodeURIComponent(pgnString);
+    const urlPlusPGN = `https://www.johntennant.com/PGNChessDrilljs/mPGNPlaybackChessboard.html?pgn=${encodedPgnString}`;
 
-      window.open(string, '_blank');
-    
-    } else {
-    
-      const pgnString = string;
-      const encodedPgnString = encodeURIComponent(pgnString);
-      const urlPlusPGN = `https://www.johntennant.com/PGNChessDrilljs/mPGNPlaybackChessboard.html?pgn=${encodedPgnString}`;
-  
-      // Load the URL with a new tab
-      window.open(urlPlusPGN, '_blank');
-  
-      // // Copy the URL to the clipboard
-      // navigator.clipboard.writeText(string).then(
-      //   function () {
-      //     console.log('Async: Copying to clipboard was successful!');
-      //   },
-      //   function (err) {
-      //     console.error('Async: Could not copy text: ', err);
-      //   }
-      // );
-    }
+    // Load the URL in the same tab
+    window.location.href = urlPlusPGN;
   }
-  
+}
