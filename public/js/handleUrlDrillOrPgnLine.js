96a1106f387f1a4c1613db362acb16dc3c3cefff
John Tennant <john@johntennant.com>
2023-05-18 11:21:45 -0400

    Change to use local storage for to pass the pgn line to the chessboard instead of using a URI component.

--- public/js/handleUrlDrillOrPgnLine.js
+++ public/js/handleUrlDrillOrPgnLine.js
@@ -1,20 +1,16 @@
 // checks if the first character of a string is a number
-// if no, then open the URL
-// if yes, then open a mPGNPlaybackChessboard.html and copy the 'string' to the clipboard
- 
+// if no, then open the URL. If yes, then store the PGN string to localStorage and open the RepertoireChessboard page
+
+
 export function openURLorPGN(string, color) {
   if (isNaN(string.charAt(0))) {
     window.location.href = string;
   } else {
     let pgnString = string;
-    pgnString = pgnString.replace(/%/g, '%25');
-    const encodedPgnString = encodeURIComponent(pgnString);
+    localStorage.setItem("pgn", pgnString); // Store PGN string to localStorage
     const encodedColor = encodeURIComponent(color);
-    // const urlPlusPGN = `https://www.johntennant.com/PGNChessDrilljs/mPGNPlaybackChessboard.html?pgn=${encodedPgnString}&color=${encodedColor}`;
-    const urlPlusPGN = `RepetoireChessboard.html?pgn=${encodedPgnString}&color=${encodedColor}`;
+    const urlPlusPGN = `RepetoireChessboard.html?color=${encodedColor}`;
     // Load the URL in the same tab
     window.location.href = urlPlusPGN;
-    // window.open(urlPlusPGN, '_blank');
   }
 }
-
